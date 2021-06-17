97e2cfb3cae3704af054d0467b1c44726bdf5fc0
Andrew Brown <andrew.brown@bom.gov.au>
2021-06-17 18:11:02 +1000

    Add lat and lon grids to file, add bbox as an attribute

--- tint/write_griddata.py
+++ tint/write_griddata.py
@@ -6,6 +6,7 @@ import datetime
 import math
 
 def Setup_h5File(grid1,outdir):
+    #TODO EDIT GLOABL ATTRS
     savef=h5py.File(outdir,'w')
     savef.attrs['proj']=np.string_('pyart_aeqd')
     savef.attrs['orig_field_name']=np.string_('reflectivity_cal_cor')
@@ -33,6 +34,17 @@ def Setup_h5File(grid1,outdir):
     savef.attrs['site_name']=np.string_('Melb') #will need to be modified if you expand
     savef.attrs['state']=np.string_('VIC')
     savef.attrs['country']=np.string_('Australia')
+
+    #Save grid lat, lon as level 1 groups, for reconstruction later
+    lon, lat = grid1.get_point_longitude_latitude()
+    lat_group = savef.create_group("lat")
+    lon_group = savef.create_group("lon")
+    lat_group.create_dataset('lat',
+			    shape=lat.shape,
+			    data=lat)
+    lon_group.create_dataset('lon',
+			    shape=lon.shape,
+			    data=lon)
     return savef
 
 def write_griddata(savef,image1,grid1,field,current_objects,record,obj_props):
@@ -42,15 +54,13 @@ def write_griddata(savef,image1,grid1,field,current_objects,record,obj_props):
     scan_num = [record.scan] * nobj
     uids = current_objects['uid']
     dts=record.time
-    outtime=dts.strftime('%H%M%S')
+    outtime=dts.strftime('%Y%m%d%H%M%S')
     ttgroup=savef.create_group(outtime)
     for IN in np.arange(nobj):
         objIN=obj_props['id1'][IN]
         uid=uids[IN]
         uidgroup=ttgroup.create_group(uid)
         boxIN=np.squeeze(obj_props['bbox'][IN])
-        refvol=grid1.fields[field]['data'] 
-        cellrefvol=refvol[:,boxIN[0]:boxIN[2],boxIN[1]:boxIN[3]]
         cellimage=image1[boxIN[0]:boxIN[2],boxIN[1]:boxIN[3]] 
         cellmasked=np.ma.masked_equal(cellimage,objIN)#careful in reconstruction. other cells may show as 0s in the frame
         cellmask=cellmasked.mask
@@ -58,14 +68,19 @@ def write_griddata(savef,image1,grid1,field,current_objects,record,obj_props):
         writecellmask=uidgroup.create_dataset('cell_mask',
                                               shape=cellgriddims,
                                               data=cellmask.astype(int))      
-        writecellref=uidgroup.create_dataset('ref',shape=cellrefvol.shape,data=cellrefvol) 
-
-        #reset dimension names...
+        uidgroup.attrs["bbox"] = boxIN
         uidgroup['cell_mask'].dims[0].label='cell_y'
         uidgroup['cell_mask'].dims[1].label='cell_x'
-        uidgroup['ref'].dims[0].label='z'
-        uidgroup['ref'].dims[1].label='cell_y'
-        uidgroup['ref'].dims[2].label='cell_x'
         
+	#To reconstruct, use this code
+#	f = h5py.File(outdir, "r")
+#	group_id = "20151215230102/2/"
+#       bbox = f[group_id].attrs["bbox"]
+#	x = f["lon/lon"][:]
+#	y = f["lat/lat"][:]
+#       recon = np.zeros(x.shape)
+#       recon[bbox[0]:bbox[2],bbox[1]:bbox[3]] = f[group_id+"/cell_mask"][:]
+#       plt.pcolormesh(x,y,recon)
+
     return savef
 
